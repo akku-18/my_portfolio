252f058240f6669ec91551c5c1547ea46b02f594
akku-18 <akshanshgarg18@gmail.com>
2022-12-21 18:24:51 +0530

    added text typing animation

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -1,18 +1,31 @@
 import "./intro.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function Intro() {
+
+  const textRef = useRef(); 
+
+  useEffect(() => {
+    init(textRef.current,{
+      showCursor:true,
+      backDelay:1500,
+      backSpeed:60,
+      strings:["Developer", "Designer", "Thinker"], 
+    })
+  },[])
   return (
     <div className='intro' id="intro">
       <div className="left">
       <div className="imgContainer">
-          <img src="assets/me.png" alt="" />
+          <img src="assets/me.v1.png" alt="" />
         </div>
       </div>
       <div className="right">
         <div className="wrapper">
           <h2>Hi there, I'm</h2>
           <h1>Akshansh Garg</h1>
-          <h3>Freelance <span></span></h3>
+          <h3>I'm a <span ref={textRef}></span></h3>
         </div>
         <a href="#portfolio">
           <img src="assets/down.png" alt="" />
