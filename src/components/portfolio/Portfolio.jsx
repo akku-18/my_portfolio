93829c7214c41025951da51c12ab5a6b38f2e5bf
akku-18 <akshanshgarg18@gmail.com>
2022-12-21 19:14:59 +0530

    portfolio page

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,6 +2,48 @@ import "./Portfolio.scss";
 
 export default function Portfolio() {
   return (
-    <div className='portfolio' id="portfolio"></div>
-  )
+    <div className="portfolio" id="portfolio">
+      <h1>Portfolio</h1>
+      <ul>
+        <li className="active">Featured</li>
+        <li>Webapp</li>
+        <li>Mobile App</li>
+        <li>Design</li>
+        <li>Branding</li>
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        <div className="item">
+          <img src="assets/quriocity.png" className="socialImg" alt="" />
+          <h3>Design App</h3>
+        </div>
+
+        
+      </div>
+    </div>
+  );
 }
