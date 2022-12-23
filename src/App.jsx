344c497ee5f405a36e3cea7e26442c7b3237b4dd
akku-18 <akshanshgarg18@gmail.com>
2022-12-23 12:37:51 +0530

    cleaned

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import Intro from "./components/intro/Intro";
 import Portfolio from "./components/portfolio/Portfolio";
-import Testimonials from "./components/testimonials/Testimonials";
 import Topbar from "./components/topbar/Topbar";
 import Works from "./components/works/Works";
 import Contact from "./components/contact/Contact";
@@ -18,7 +17,6 @@ function App() {
         <Intro />
         <Portfolio />
         <Works />
-        {/* <Testimonials /> */}
         <Contact />
       </div>
     </div>
